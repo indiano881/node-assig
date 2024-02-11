fe116197dafe14e9187a411d6738eeb0992cbf73
Davide Baldi <d.baldi88@gmail.com>
2024-02-11 20:26:38 +0100

    save before routing

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-http.createServer((req,res), ()=>{
-    
-})
+http.createServer((req,res) => {
+    res.writeHead(200, "fea23gth club works", {"Content-type":"text/html"})
+
+
+
+    res.end();
+
+}).listen(3030,()=>console.log("server started"))
