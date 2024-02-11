b77d1fb72e5e63cc9979bc36927255b4c753ed1a
Davide Baldi <d.baldi88@gmail.com>
2024-02-11 20:44:50 +0100

    save the work-NEED to check why txt file is not desplayed

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ const url = require("url");
 const fs = require("fs");
 
 http.createServer((req,res) => {
-    res.writeHead(200, "fea23gth club works", {"Content-type":"text/html"})
-
+    res.writeHead(200, "fea23gth club works")
+    if (req.url==="/english") {
+        fs.readFile("rules_en.txt", (err,data)=>{
+            if (err) throw err;
+            res.write(data);
+            res.end();
+        }
+            
+        )
+        
+    } else if (req.url==="/italian") {
+        fs.readFile("rules_it.txt", (err,data)=>{
+            if (err) throw err;
+            res.write(data);
+            res.end();
+        })
+        
+    } else {
+        res.write("<h1> Welcome to the Fea23ght club</h1>")
+        res.end();
+    }
 
 
     res.end();
