bbe39b5149607d113251ce2507aae8237494c380
Davide Baldi <d.baldi88@gmail.com>
2024-02-15 09:13:22 +0100

    create 2 html file to make only external routing

--- server.js
+++ server.js
@@ -30,3 +30,21 @@ http.createServer((req,res) => {
     
 
 }).listen(3030,()=>console.log("server started"))
+
+//?language=english&rule=1;
+//OR /english?rule=1
+/*
+To achieve a Godkänt grade you must:
+
+Create a webserver
+Create a home page
+Create content for at least 4 routes
+At least two routes must render html
+At least two routes must display content that comes from an external file (eg txt or html)
+To achieve a Välgodkänt grade you must do the above and:
+Read queries passed in the url
+Generate different content based on the queries
+The content should be stored in external files
+
+The actual content, routes and queries are entirety up to you. For ease of assessment, I strongly recommend adding instructions on how to access your routes and queries to the home page. I need to know where I'm supposed to go!
+*/
