07094d62ff0644bf75bec9b15ad18e5a933fde7b
Davide Baldi <d.baldi88@gmail.com>
2024-02-15 10:40:04 +0100

    8 rules queries added

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const url = require("url");
 const fs = require("fs");
 
 http.createServer((req,res) => {
-//queries???
+//queries-do we need this part????
 const adress_query= "http://fea23ght-club.se/english?rule=1";
 const fullAdress= url.parse(adress_query,true);
-
-
-
 //end of queries
 
 
@@ -18,17 +15,39 @@ const fullAdress= url.parse(adress_query,true);
         let queries= url.parse(req.url, true).query;
         if (queries.rule==="1") {
             res.write("Rule n.1");
-        }
+            res.end();
+        } else if (queries.rule=="2") {
+            res.write("Rule n.2");
+            res.end();
+        }  else if (queries.rule=="3") {
+            res.write("Rule n.3");
+            res.end();
+        }  else if (queries.rule=="4") {
+            res.write("Rule n.4");
+            res.end();
+        } else if (queries.rule=="5") {
+            res.write("Rule n.5");
+            res.end();
+        }  else if (queries.rule=="6") {
+            res.write("Rule n.6");
+            res.end();
+        }  else if (queries.rule=="7") {
+            res.write("Rule n.7");
+            res.end();
+        } else if (queries.rule=="8") {
+            res.write("Rule n.8");
+            res.end();
+        } 
         console.log(queries.rule);
 
         fs.readFile("rules_en.txt", (err,data)=>{
             if (err) throw err;
             res.write(data);
-            
+            res.end();
         }
             
         )
-        res.end();
+        
     } else if (req.url==="/italian") {
         res.writeHead(200, "fea23gth club works",{"Content-Type":"text/plain"})
         fs.readFile("rules_it.txt", (err,data)=>{
